5329a66725fc8c33a47b8fc36fd3b9a8ea911245
wguzik <wojciech.guzik@gmail.com>
2024-10-13 18:20:48 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
     </head>
     <body>
       <div class="container">
-        <h1>Hello World from ${regionName}</h1>
+        <h1>Hello World from ${regionName}, yay!</h1>
       </div>
     </body>
     </html>
