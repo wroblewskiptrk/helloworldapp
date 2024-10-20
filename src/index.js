6711a5cf9ed3affd088e2824d4bf83a50e26e823
wroblewskiptrk <wroblewskiptrk@gmail.com>
2024-10-20 14:01:36 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-cons app = express();
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
