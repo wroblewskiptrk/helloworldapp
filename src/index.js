da9ef053028a21ddb57105f84e0260ee4f9d4de1
wroblewskiptrk <wroblewskiptrk@gmail.com>
2024-10-20 13:44:39 +0200

    szybki fix piatek 14;30

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const app = express();
+cons app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
